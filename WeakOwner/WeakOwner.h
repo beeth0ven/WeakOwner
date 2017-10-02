0369dd41b1f4128ba9fa97389b5b0234c3d7370e
beeth0ven <beeth0vendev@163.com>
2017-10-02 20:41:35 +0800

    Initial Commit

---
+++ WeakOwner/WeakOwner.h
@@ -0,0 +1,19 @@
+//
+//  WeakOwner.h
+//  WeakOwner
+//
+//  Created by luojie on 2017/10/2.
+//  Copyright © 2017年 LuoJie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WeakOwner.
+FOUNDATION_EXPORT double WeakOwnerVersionNumber;
+
+//! Project version string for WeakOwner.
+FOUNDATION_EXPORT const unsigned char WeakOwnerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeakOwner/PublicHeader.h>
+
+
